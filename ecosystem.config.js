9a0ad442b94a18b57103cf2044d0e6343dc9fa9d
960765732@qq.com <960765732@qq.com>
2017-03-07 16:28:56 +0800

    sw

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       user : "root",
       host : "123.56.99.220",
       ref  : "origin/master",
-      repo : "git@github.com:xhd/blog.git",
+      repo : "https://github.com/xhd/blog",
       path : "/var/www/production",
       "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
     }
